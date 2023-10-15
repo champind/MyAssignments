cd4c8d3e10cbda7fc5d8fcdbb0a715e086743e3a
champind <147971928+champind@users.noreply.github.com>
2023-10-15 13:38:33 +0530

    Add files via upload
    day1 Home Assignments 1. Demonstrating Variables in Browser class 2. Demonstrating If-Else Condition in FindNumber class

---
+++ day1/Browser.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Browser {
+public static void main(String[] args) {
+	float browserVersion = 117.0f;
+	String browserName = "chrome";
+	boolean pageLoaded = true;
+	int noOfBrowser = 1;
+	char browserLogo = 'C';
+
+	System.out.println(browserVersion+" "+browserName+" "+pageLoaded+" "+noOfBrowser+" "+browserLogo );
+
+}
+
+}
---
+++ day1/FindNumber.java
@@ -0,0 +1,16 @@
+package week1.day1;
+public class FindNumber {
+public static void main(String[] args) {
+	
+	int a = 45;
+	
+	if (a>0) {
+		System.out.println("The number is positive");	
+	}
+
+	else {
+		System.out.println("The number is negative");
+	}
+}	
+	
+}
